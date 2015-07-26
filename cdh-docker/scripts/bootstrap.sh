2b14247969f0aaec93ff8b7661e85dc11f8e9450
SJTU_iiot <hello@sjtu.edu.cn>
2015-07-26 16:34:47 +0800

    fix hadoop start by chmod

--- cdh-docker/scripts/bootstrap.sh
+++ cdh-docker/scripts/bootstrap.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
+hdfs namenode -format
+
+mkdir -p /var/lib/hadoop-hdfs/cache/root/
+mkdir -p /var/lib/hadoop-hdfs/cache/root/name
+mkdir -p /var/lib/hadoop-hdfs/cache/root/namesecondary
+mkdir -p /var/lib/hadoop-hdfs/cache/root/data
+chmod -R 1777 /var/lib/hadoop-hdfs/
+
 # start hadoop nodes
 service hadoop-hdfs-namenode start
 service hadoop-hdfs-datanode start
 
-sudo -u hdfs hadoop fs -mkdir -p /tmp/hadoop-yarn/staging/history/done_intermediate
-sudo -u hdfs hadoop fs -chown -R mapred:mapred /tmp/hadoop-yarn/staging
-sudo -u hdfs hadoop fs -chmod -R 1777 /tmp
-sudo -u hdfs hadoop fs -mkdir -p /var/log/hadoop-yarn
-sudo -u hdfs hadoop fs -chown yarn:mapred /var/log/hadoop-yarn
+#sudo -u hdfs hadoop fs -mkdir -p /tmp/hadoop-yarn/staging/history/done_intermediate
+hadoop fs -mkdir -p /tmp/hadoop-yarn/staging/history/done_intermediate
+#hadoop fs -chown -R mapred:mapred /tmp/hadoop-yarn/staging
+hadoop fs -chmod -R 1777 /tmp
+hadoop fs -mkdir -p /var/log/hadoop-yarn
+hadoop fs -chown yarn:mapred /var/log/hadoop-yarn
 
 # start yarn
 service hadoop-yarn-resourcemanager start
 service hadoop-yarn-nodemanager start
 service hadoop-mapreduce-historyserver start
 
-sudo -u hdfs hadoop fs -mkdir -p /user/hdfs
-sudo -u hdfs hadoop fs -chown hdfs /user/hdfs
+hadoop fs -mkdir -p /user/hdfs
+#hadoop fs -chown hdfs /user/hdfs
 
 # start spark-history-server
-sudo -u hdfs hadoop fs -mkdir /user/spark
-sudo -u hdfs hadoop fs -mkdir /user/spark/applicationHistory 
-sudo -u hdfs hadoop fs -chown -R spark:spark /user/spark
-sudo -u hdfs hadoop fs -chmod 1777 /user/spark/applicationHistory
+hadoop fs -mkdir /user/spark
+hadoop fs -mkdir /user/spark/applicationHistory 
+#hadoop fs -chown -R spark:spark /user/spark
+hadoop fs -chmod 1777 /user/spark/applicationHistory
 
-sudo service spark-history-server start
+service spark-history-server start
