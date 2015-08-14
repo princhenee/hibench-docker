510cc49616dd6c9eb2c885e35d64e62c7f46e1af
ge.chen <ge.chen@intel.com>
2015-08-14 21:24:08 +0800

    fix ssh; cdh second version

--- cdh-docker/scripts/bootstrap.sh
+++ cdh-docker/scripts/bootstrap.sh
@@ -1,37 +1,38 @@
 #!/bin/bash
 
-rm -fr /var/lib/hadoop-hdfs/cache/root/*
-hdfs namenode -format
+# start ssh service
+service ssh restart
 
-#mkdir -p /var/lib/hadoop-hdfs/cache/root/
-#mkdir -p /var/lib/hadoop-hdfs/cache/root/name
-#mkdir -p /var/lib/hadoop-hdfs/cache/root/namesecondary
-#mkdir -p /var/lib/hadoop-hdfs/cache/root/data
-chmod -R 777 /var/lib/hadoop-hdfs/cache/root/
+# remove existing data directories
+rm -fr /var/lib/hadoop-hdfs/*
+sudo -u hdfs hdfs namenode -format
+
+sudo -u hdfs chmod -R 777 /var/lib/hadoop-hdfs
 
 # start hadoop nodes
 service hadoop-hdfs-namenode start
 service hadoop-hdfs-datanode start
 
-#sudo -u hdfs hadoop fs -mkdir -p /tmp/hadoop-yarn/staging/history/done_intermediate
-hadoop fs -mkdir -p /tmp/hadoop-yarn/staging/history/done_intermediate
-#hadoop fs -chown -R mapred:mapred /tmp/hadoop-yarn/staging
-hadoop fs -chmod -R 1777 /tmp
-hadoop fs -mkdir -p /var/log/hadoop-yarn
-#hadoop fs -chown yarn:mapred /var/log/hadoop-yarn
+#sudo -u hdfs hdfs dfs -mkdir -p /tmp/hadoop-yarn/staging/history/done_intermediate
+hdfs dfs -mkdir -p /tmp/hadoop-yarn/staging/history/done_intermediate
+hdfs dfs -chown -R mapred:mapred /tmp/hadoop-yarn/staging
+hdfs dfs -chmod -R 1777 /tmp
+hdfs dfs -mkdir -p /var/log/hadoop-yarn
+hdfs dfs -chown yarn:mapred /var/log/hadoop-yarn
 
 # start yarn
 service hadoop-yarn-resourcemanager start
 service hadoop-yarn-nodemanager start
 service hadoop-mapreduce-historyserver start
 
-hadoop fs -mkdir -p /user/hdfs
-#hadoop fs -chown hdfs /user/hdfs
+hdfs dfs -mkdir -p /user/hdfs
+hdfs dfs -chown hdfs:hdfs /user/hdfs
 
 # start spark-history-server
-hadoop fs -mkdir /user/spark
-hadoop fs -mkdir /user/spark/applicationHistory 
-#hadoop fs -chown -R spark:spark /user/spark
-hadoop fs -chmod 1777 /user/spark/applicationHistory
+hdfs dfs -mkdir /user/spark
+hdfs dfs -mkdir /user/spark/applicationHistory 
+hdfs dfs -chown -R spark:spark /user/spark
+hdfs dfs -chmod 1777 /user/spark/applicationHistory
 
 service spark-history-server start
+${SPARK_HOME}/sbin/start-all.sh
--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -4,15 +4,34 @@ CUR_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 HOME_DIR=${CUR_DIR}/..
 
 
+function build-base() {
 echo "============================================"
 echo "   Start building hibench-base image...     "
 echo "============================================"
 bash ${CUR_DIR}/gen_base_dockerfile.sh
 sudo docker build -t hibench-base ${HOME_DIR}/base/
+}
 
-
+function second-step() {
 echo "============================================"
 echo "   Start building hibench-docker image...   "
 echo "============================================"
-sudo docker build -t hibench-docker ${HOME_DIR}/hadoop-spark/
+}
+
+case "$1" in
+  "cdh")
+     build-base
+     second-step
+     sudo docker build -t hibench-docker ${HOME_DIR}/cdh-docker/
+     ;;
+  "standalone")
+     build-base
+     second-step
+     sudo docker build -t hibench-docker ${HOME_DIR}/hadoop-spark/
+     ;;
+  *)
+     echo "Usage $0 {cdh|standalone}"
+     exit 1
+     ;;
+esac
 
