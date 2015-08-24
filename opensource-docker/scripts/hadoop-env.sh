b2216e4ab0248a172e594b9f06a13101cb308ab0
ge.chen <ge.chen@intel.com>
2015-08-24 18:56:56 +0800

    add apache license

--- opensource-docker/scripts/hadoop-env.sh
+++ opensource-docker/scripts/hadoop-env.sh
@@ -13,23 +13,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-#!/bin/bash
-
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-echo "#=================================================#"
-echo "     This script first build the images            "
-echo "       and then launch the container               "
-echo "       with all dependencies installed             "
-echo "#=================================================#"
-
-${DIR}/build-docker.sh $1
-if [ $? == 1 ]; then
-    exit 1;
-fi
-
-WL_NAME=wordcount
-# run HiBench workload example
-# Format: sudo docker run (-v "LocalLargeDiskDir:/usr/loal"-it) hibench-hadoop-spark /bin/bash /root/HiBench/workloads/<workload-name>/prepare/prepare.sh
-sudo docker run -ti hibench-docker
+export JAVA_HOME=/usr/lib/jvm/java-7-oracle
